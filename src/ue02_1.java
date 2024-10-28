231ca8cbdfb990738ff525be2b8ced346f9597b7
annaamayr <s2410238039@fhooe.at>
2024-10-28 15:37:04 +0100

    first commit

--- src/ue02_1.java
+++ src/ue02_1.java
@@ -1,7 +1,22 @@
-import libraries.Out;
-
 public class ue02_1 {
     public static void main(String[] args) {
         Out.println("Hello, World!");
+ 
+        Out.print("x: "); int x = In.readInt();
+        Out.print("y: "); int y = In.readInt();
+        Out.print("z: "); int z = In.readInt();
+        int max;
+
+        if (x > y && x > z) {
+                max=x;
+        }   else if (y > x && y > z) {
+                max=y;
+        }   else {
+                max=z;
+        }
+        
+        Out.print("Maximum: ");
+        Out.println(max);
     }
+
 }
