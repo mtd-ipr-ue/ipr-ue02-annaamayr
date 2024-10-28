996f51d5eabe91b532a8c457cf0f8b26630a7aaa
annaamayr <s2410238039@fhooe.at>
2024-10-28 15:41:29 +0100

    second ue02_1.java

--- src/ue02_1.java
+++ src/ue02_1.java
@@ -2,9 +2,12 @@ public class ue02_1 {
     public static void main(String[] args) {
         Out.println("Hello, World!");
  
-        Out.print("x: "); int x = In.readInt();
-        Out.print("y: "); int y = In.readInt();
-        Out.print("z: "); int z = In.readInt();
+        Out.print("x: ");
+        int x = In.readInt();
+        Out.print("y: ");
+        int y = In.readInt();
+        Out.print("z: ");
+        int z = In.readInt();
         int max;
 
         if (x > y && x > z) {
