d20e54814595aefad2d94408b4f04a3c16b77025
annaamayr <s2410238039@fhooe.at>
2024-11-06 11:31:12 +0100

    Update ue02_3.java

--- src/ue02_3.java
+++ src/ue02_3.java
@@ -1,7 +1,26 @@
+import libraries.In;
 import libraries.Out;
 
 public class ue02_3 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+
+            Out.print("x: ");
+            double x = In.readInt();
+            Out.print("y: ");
+            int y = In.readInt();
+                
+                
+                double radius = 4.0;
+                
+                
+                double distance = Math.sqrt(x * x + y * y);
+                
+                
+                if (distance <= radius) {
+                    System.out.println("Point is within the circle");
+                } else {
+                    System.out.println("Point is outside the circle.");
+                }
+            }
+        
     }
-}
